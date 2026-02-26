1a631bd6217732dd7689297c56bf5a0f3a533435
Jordan Barber <jordanbarber@Jordans-MacBook-Air.local>
2026-02-26 15:07:20 -0500

    move scatter

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -32,15 +32,15 @@
     <div class="hidden lg:block w-[40px] h-[140px] absolute bottom-0 right-[280px] bg-linear-to-r from-teal-500 to-teal-400 animate-pulse duration-1000 delay-1000"></div>
 
     <!-- Scatter Plot Background -->
-    <div class="hidden z-10 lg:block absolute bottom-[40px] left-[240px] w-[8px] h-[8px] rounded-full bg-teal-300 opacity-20 mix-blend-overlay animate-pulse duration-1200"></div>
-    <div class="hidden z-10 lg:block absolute bottom-[90px] left-[300px] w-[12px] h-[12px] rounded-full bg-teal-200/70 blur-[1px] opacity-10 mix-blend-overlay animate-pulse duration-1400 delay-200"></div>
-    <div class="hidden z-10 lg:block absolute bottom-[60px] left-[360px] w-[6px] h-[6px] rounded-full bg-teal-400 opacity-20 mix-blend-overlay animate-pulse duration-1000 delay-400"></div>
-    <div class="hidden z-10 lg:block absolute bottom-[130px] left-[260px] w-[10px] h-[10px] rounded-full bg-teal-300/70 opacity-10 mix-blend-overlay animate-pulse duration-1300 delay-600"></div>
-    <div class="hidden z-10 lg:block absolute bottom-[170px] left-[340px] w-[14px] h-[14px] rounded-full bg-gradient-to-r from-teal-400 to-teal-200 opacity-5 blur-[2px] mix-blend-overlay animate-pulse duration-1500 delay-800"></div>
-    <div class="hidden z-10 lg:block absolute bottom-[110px] left-[420px] w-[7px] h-[7px] rounded-full bg-teal-200 opacity-10 blur-[2px] mix-blend-overlay animate-pulse duration-1200 delay-1000"></div>
-    <div class="hidden z-10 lg:block absolute bottom-[30px] left-[400px] w-[9px] h-[9px] rounded-full bg-teal-300 opacity-15 mix-blend-overlay animate-pulse duration-1600 delay-1200"></div>
-    <div class="hidden z-10 lg:block absolute bottom-[200px] left-[280px] w-[11px] h-[11px] rounded-full bg-teal-200/80 opacity-5 blur-[1px] mix-blend-overlay animate-pulse duration-1300 delay-1400"></div>
-    <div class="hidden z-10 lg:block absolute bottom-[150px] left-[220px] w-[6px] h-[6px] rounded-full bg-teal-400 opacity-15 mix-blend-overlay animate-pulse duration-1100 delay-1600"></div>
+    <div class="hidden z-10 lg:block absolute bottom-[0px] left-[360px] w-[8px] h-[8px] rounded-full bg-teal-300 opacity-20 mix-blend-overlay animate-pulse duration-1200"></div>
+    <div class="hidden z-10 lg:block absolute bottom-[50px] left-[420px] w-[12px] h-[12px] rounded-full bg-teal-200/70 blur-[1px] opacity-10 mix-blend-overlay animate-pulse duration-1400 delay-200"></div>
+    <div class="hidden z-10 lg:block absolute bottom-[10px] left-[480px] w-[6px] h-[6px] rounded-full bg-teal-400 opacity-20 mix-blend-overlay animate-pulse duration-1000 delay-400"></div>
+    <div class="hidden z-10 lg:block absolute bottom-[90px] left-[380px] w-[10px] h-[10px] rounded-full bg-teal-300/70 opacity-10 mix-blend-overlay animate-pulse duration-1300 delay-600"></div>
+    <div class="hidden z-10 lg:block absolute bottom-[130px] left-[460px] w-[14px] h-[14px] rounded-full bg-gradient-to-r from-teal-400 to-teal-200 opacity-5 blur-[2px] mix-blend-overlay animate-pulse duration-1500 delay-800"></div>
+    <div class="hidden z-10 lg:block absolute bottom-[70px] left-[540px] w-[7px] h-[7px] rounded-full bg-teal-200 opacity-10 blur-[2px] mix-blend-overlay animate-pulse duration-1200 delay-1000"></div>
+    <div class="hidden z-10 lg:block absolute bottom-[0px] left-[520px] w-[9px] h-[9px] rounded-full bg-teal-300 opacity-15 mix-blend-overlay animate-pulse duration-1600 delay-1200"></div>
+    <div class="hidden z-10 lg:block absolute bottom-[160px] left-[400px] w-[11px] h-[11px] rounded-full bg-teal-200/80 opacity-5 blur-[1px] mix-blend-overlay animate-pulse duration-1300 delay-1400"></div>
+    <div class="hidden z-10 lg:block absolute bottom-[110px] left-[340px] w-[6px] h-[6px] rounded-full bg-teal-400 opacity-15 mix-blend-overlay animate-pulse duration-1100 delay-1600"></div>
 
     <!-- Circle -->
     <div class="hidden z-0 sm:block rounded-full bg-linear-to-r from-teal-300 to-teal-500 w-72 h-72 absolute pie-circle"></div>
